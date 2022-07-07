783a89991e51ef9044e8abb8bc09b4464ef9736a
Imarino4 <ignacio.martacos@gmail.com>
2022-07-07 10:27:42 -0700

    Final

--- _printf.c
+++ _printf.c
@@ -19,7 +19,9 @@ int _printf(const char *format, ...)
 			if (format[i] == '%')
 			{
 				if (format[i + 1] == '\0')
-				       return (-1);	
+				{
+					return (-1);
+				}
 				i++;
 				if (format[i] == '%')
 				{
