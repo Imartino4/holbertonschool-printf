4249405f6dc6aa9640416bb1f040e5dfbcc53d79
Imarino4 <ignacio.martacos@gmail.com>
2022-07-08 09:10:54 -0700

    adding int min

--- _int.c
+++ _int.c
@@ -10,17 +10,22 @@
  */
 int _int(va_list ptr)
 {
-	int i = 0, c = 0, n, n_dig = 0, div, num;
-	/*unsigned int num2;*/
+	int c = 0, n_dig = 0, div, num;
+	unsigned int num2, n, i = 0;
 
 	num = va_arg(ptr, int);
 	if (num < 0)
 	{
 		_putchar('-');
-		num = num * -1;
+		num2 = num * -1;
 		c++;
+
 	}
-	n = num;
+	else if (num == 0)
+		_putchar (num + '0');
+	else
+		num2 = num;
+	n = num2;
 	while (n > 0)
 	{
 		n_dig++;
@@ -30,7 +35,7 @@ int _int(va_list ptr)
 	for (i= 0; i < n; i++, n_dig--)
 	{
 		div = _pow_integers(10, n_dig - 1);
-		_putchar((num / div) % 10 + '0');
+		_putchar((num2 / div) % 10 + '0');
 		c++;
 	}
 	return (c);
