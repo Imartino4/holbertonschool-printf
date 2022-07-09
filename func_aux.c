2c7d15fb7ccb1f70113b6bac3855d9ef5fbeb452
Imarino4 <ignacio.martacos@gmail.com>
2022-07-08 21:03:39 -0700

    func aux bin

--- func_aux.c
+++ func_aux.c
@@ -11,7 +11,7 @@ int func_aux(char s, va_list ptr)
 {
 	pf_aux pf_aux[] = {
 		{'c', _char}, {'s', _string}, {'i', _int},
-		{'d', _int}, {'\0', NULL}
+		{'d', _int}, {'b', _bin}, {'\0', NULL}
 	};
 	int j = 0, k = 0;
 
